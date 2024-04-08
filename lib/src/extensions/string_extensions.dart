627f1ad83112dfea8d04b9d50126b7f647622016
Dominik Krajcer <dom.kraj@gmail.com>
2024-04-08 14:53:23 +0200

    add missing docs

--- lib/src/extensions/string_extensions.dart
+++ lib/src/extensions/string_extensions.dart
@@ -5,13 +5,21 @@ extension StringExtensions on String {
   /// Example: ':smile:' becomes 'smile'.
   String removeColons() => replaceAll(RegExp(r'^:|:$'), '');
 
+  /// Checks if the string contains the specified [other] string, ignoring case.
+  /// Returns `true` if the string contains [other] ignoring case, otherwise `false`.
+  /// Example: 'Hello World'.containsIgnoreCase('hello') returns true.
   bool containsIgnoreCase(String other) {
     return toLowerCase().contains(other.toLowerCase());
   }
 
+  /// Checks if the string is equal to the specified [other] string, ignoring case.
+  /// Returns `true` if the string is equal to [other] ignoring case, otherwise `false`.
+  /// Example: 'HELLO'.equalsIgnoreCase('hello') returns true.
   bool equalsIgnoreCase(String other) {
     return toLowerCase() == other.toLowerCase();
   }
 
+  /// Wraps the string with leading and trailing colons.
+  /// Example: 'smile'.wrapWithColons() returns ':smile:'.
   String wrapWithColons() => ':$this:';
 }
